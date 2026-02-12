6319ae8dff1f0ed1d1874524292fca363183759f
Aaron Elmquist <aaron.elmquist@protonmail.com>
2026-02-12 17:58:25 -0600

    adds health checks for the backend

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -6,9 +6,11 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
-var targetUrls []*url.URL = []*url.URL{
+var configUrls []*url.URL = []*url.URL{
 	{
 		Scheme: "http",
 		Host:   "backend6:6060",
@@ -23,31 +25,59 @@ var targetUrls []*url.URL = []*url.URL{
 	},
 }
 
+var servers atomic.Pointer[[]*url.URL]
+
 var server int = 0
 var mu sync.Mutex
 
 func main() {
-	proxy := &httputil.ReverseProxy{
-		Rewrite: func(r *httputil.ProxyRequest) {
-			mu.Lock()
-			r.SetURL(targetUrls[server])
-			server = (server + 1) % len(targetUrls)
-			mu.Unlock()
-		},
-	}
+	servers.Store(&configUrls)
+	go func() {
+		for range time.Tick(10 * time.Second) {
+			var healthy []*url.URL
+			for _, u := range configUrls {
+				if isHealthy(u.Host) {
+					healthy = append(healthy, u)
+				}
+			}
+			servers.Store(&healthy)
+		}
+	}()
 
 	err := http.ListenAndServe(":9090", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//TODO: maybe use atomic instead?
-		mu.Lock()
-		if len(targetUrls) == 0 {
-			mu.Unlock()
+		s := *servers.Load()
+		if len(s) == 0 {
 			w.WriteHeader(http.StatusServiceUnavailable)
 			return
 		}
-		mu.Unlock()
+		proxy := &httputil.ReverseProxy{
+			Rewrite: func(r *httputil.ProxyRequest) {
+				mu.Lock()
+				server = server % len(s)
+				r.SetURL(s[server])
+				server = (server + 1) % len(s)
+				mu.Unlock()
+			},
+		}
 		proxy.ServeHTTP(w, r)
 	}))
 	if err != nil {
 		log.Fatal("failed to start server")
 	}
+}
+
+func isHealthy(host string) bool {
+	req, err := http.NewRequest("GET", "http://"+host+"/api/healthz/", nil)
+	if err != nil {
+		return false
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK
 }
