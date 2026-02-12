2abf26056dd1067290eef565ade0e69c14bd58af
Aaron Elmquist <aaron.elmquist@protonmail.com>
2026-02-11 18:14:58 -0600

    adds healthz end toggle endpoints

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,11 +1,38 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+var isHealthy bool = true
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello from backend"))
+	})
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		healthy := map[string]bool{"healthy": isHealthy}
+		data, err := json.Marshal(healthy)
+		if err != nil {
+			log.Printf("failed to marshall data %v: %v", healthy, err)
+			data = []byte("failed to marshall data")
+		}
+
+		if isHealthy && err == nil {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		w.Write(data)
+	})
+	mux.HandleFunc("GET /api/toggle-health", func(w http.ResponseWriter, r *http.Request) {
+		isHealthy = !isHealthy
 	})
 
 	http.ListenAndServe(":9090", mux)
